6a0042dbc879b1e8ed4864fb243d0b747bbc00f3
Anna Sonchak <kisukeurahara1328@gmail.com>
2023-05-19 14:19:33 +0300

    Category model

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -4,5 +4,16 @@ namespace Personal_Finance_Manager.Models
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<Category> Categories { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+            Database.EnsureCreated(); // Creating DB for the first time
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id_category = 1, Name = "Food", Description = "Food cost"}
+            );
+        }
     }
 }
---
+++ Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Personal_Finance_Manager.Models
+{
+    public class Category
+    {
+        public int Id_category { get; set; }
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+    }
+}
