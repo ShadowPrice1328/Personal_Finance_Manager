85409e65eb1f869ad06b4713aea229e95fbddd17
Anna Sonchak <kisukeurahara1328@gmail.com>
2023-05-23 23:18:02 +0300

    Transaction module

--- Controllers/TransactionsController.cs
+++ Controllers/TransactionsController.cs
@@ -1,12 +1,48 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Personal_Finance_Manager.Models;
 
 namespace Personal_Finance_Manager.Controllers
 {
     public class TransactionsController : Controller
     {
+        private readonly AppDbContext _appDbContext;
+
+        public TransactionsController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
         public IActionResult Index()
         {
+            var allTransactions = _appDbContext.Transactions.ToList();
+            return View(allTransactions);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var allCategories = _appDbContext.Categories.ToList();
+
+            if (allCategories != null)
+            {
+                ViewData["Categories"] = allCategories;
+                return View(new Transaction());
+            }
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Transaction model)
+        {
+            if (ModelState.IsValid)
+            {
+                _appDbContext.Transactions.Add(model);
+                _appDbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(model);
         }
     }
 }
---
+++ Models/TransactionViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Personal_Finance_Manager.Models
+{
+    public class TransactionViewModel
+    {
+        public List<Category> Categories { get; set; }
+        public Transaction Transaction { get; set; }
+    }
+}
