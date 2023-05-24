262770e0a2b1a999431933882affd027cb8d82cd
Anna Sonchak <kisukeurahara1328@gmail.com>
2023-05-24 20:06:11 +0300

    Trying to manage error with password

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -1,17 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Personal_Finance_Manager.Models
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public AppDbContext(DbContextOptions<AppDbContext> options, IHttpContextAccessor httpContextAccessor) : base(options)
         {
-            // Ensure the database is created
             Database.EnsureCreated();
         }
+
         public DbSet<Category> Categories { get; set; }
         public DbSet<Transaction> Transactions { get; set; }
-
     }
 }
