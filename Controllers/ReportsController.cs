1e1136e3815bd3c52ad08df1d3d032a273b7a03a
Anna Sonchak <kisukeurahara1328@gmail.com>
2023-05-28 00:00:37 +0300

    ReportsController comments

--- Controllers/ReportsController.cs
+++ Controllers/ReportsController.cs
@@ -16,6 +16,7 @@ namespace Personal_Finance_Manager.Controllers
             _appDbContext = appDbContext;
         }
 
+        // Returning to Index if problem with connection
         public override void OnActionExecuting(ActionExecutingContext context)
         {
             if (!_appDbContext.Database.CanConnect())
@@ -28,18 +29,22 @@ namespace Personal_Finance_Manager.Controllers
 
         public IActionResult Index()
         {
+            // Adding tables' data to model
             var viewModel = new ReportViewModel
             {
                 Transactions = _appDbContext.Transactions.ToList(),
                 Categories = _appDbContext.Categories.ToList()
             };
+            // Transfering tables to Index
             return View(viewModel);
         }
         [HttpPost]
         public IActionResult DayByDay(ReportViewModel model)
         {
+            // Receiving transactions by conditions from form
             List<Transaction> allTransactions = GetTransactions(model);
 
+            // Creating viewModel with all tables and needed transactions
             ReportWithoutCategoryViewModel viewModel = CreateReportWithoutCategoryViewModel(model, allTransactions);
             return View("Graph", viewModel);
         }
@@ -47,8 +52,10 @@ namespace Personal_Finance_Manager.Controllers
         [HttpPost]
         public IActionResult Generate(ReportViewModel model)
         {
+            // Receiving transactions by conditions from form
             List<Transaction> allTransactions = GetTransactions(model);
 
+            // Checking if Category is selected
             if (!string.IsNullOrEmpty(model.Category))
             {
                 ReportWithCategoryViewModel viewModel = CreateReportWithCategoryViewModel(model, allTransactions);
@@ -63,7 +70,10 @@ namespace Personal_Finance_Manager.Controllers
 
         private ReportWithoutCategoryViewModel CreateReportWithoutCategoryViewModel(ReportViewModel model, List<Transaction> allTransactions)
         {
+            // Sum of all transactions
             decimal allCost = allTransactions.Sum(t => t.Cost);
+
+            // Creating a Dictionary <Category, Cost>
             var categoryCosts = CalculateCategoryCosts(allTransactions);
 
             var viewModel = new ReportWithoutCategoryViewModel
@@ -82,11 +92,15 @@ namespace Personal_Finance_Manager.Controllers
 
         private ReportWithCategoryViewModel CreateReportWithCategoryViewModel(ReportViewModel model, List<Transaction> allTransactions)
         {
+            // Selecting transactions with selected category
             List<Transaction> selectedTransactions = allTransactions
                 .Where(t => t.Category == model.Category)
                 .ToList();
 
+            // Counting total Cost of transactions with selected category
             decimal catCost = selectedTransactions.Sum(t => t.Cost);
+
+            // Creating a Dictionary <Category, Cost>
             var categoryCosts = CalculateCategoryCosts(allTransactions);
 
             var viewModel = new ReportWithCategoryViewModel
@@ -104,6 +118,7 @@ namespace Personal_Finance_Manager.Controllers
         }
         private List<Transaction> GetTransactions(ReportViewModel model)
         {
+            // Transfering transactions of the selected period of the selected Type
             return _appDbContext.Transactions
                 .Where(t => t.Date >= model.FirstDate &&
                             t.Date <= model.LastDate &&
@@ -111,8 +126,10 @@ namespace Personal_Finance_Manager.Controllers
                 .ToList();
         }
 
+        // Dictionary <Category, Cost>
         private Dictionary<string, decimal> CalculateCategoryCosts(List<Transaction> transactions)
         {
+            // Selecting transactions' categories
             var categories = transactions
                 .Select(t => t.Category)
                 .Distinct()
@@ -120,8 +137,10 @@ namespace Personal_Finance_Manager.Controllers
 
             var categoryCosts = new Dictionary<string, decimal>();
 
+            // Pushing keys and values to Dictionary <Category, Cost>
             foreach (var category in categories)
             {
+                // Counting total cost of each category
                 decimal cost = transactions
                     .Where(t => t.Category == category)
                     .Sum(t => t.Cost);
@@ -129,6 +148,7 @@ namespace Personal_Finance_Manager.Controllers
                 categoryCosts.Add(category, cost);
             }
 
+            // Returning Dictionary of each categories' total cost
             return categoryCosts;
         }
     }
